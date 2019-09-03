9fa846b8ecbaac7c47fce2d687664bfae584bde0
DragstylerNL <dragstylernl@gmail.com>
2019-09-03 11:28:27 +0200

    Mainmenu almost complete

---
+++ PRJCT_SPC_NVDRS/Assets/_Scripts/LineH.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineH : MonoBehaviour
+{
+
+    public float _speed = 1;
+
+    private Material m_Material;
+
+    private void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.back * _speed * Time.deltaTime;
+
+        if(transform.position.z <= 0) { transform.position += Vector3.forward * 50; }
+        m_Material.color = new Color(1, (transform.position.z/ 50 ), ((transform.position.z / 50) / 2) + 0.5f, 1);
+    }
+}
---
+++ PRJCT_SPC_NVDRS/Assets/_Scripts/SynthWaveBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynthWaveBuilder : MonoBehaviour
+{
+
+    public GameObject _line;
+    public GameObject _lineH;
+
+    public int _verticalOnesNeedend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnVertical();
+        SpawnHorizontal();
+    }
+
+    void SpawnVertical()
+    {
+        for(int i = 0; i < _verticalOnesNeedend; i++)
+        {
+            GameObject currentLine = Instantiate(_line);
+            currentLine.transform.localPosition = new Vector3(-(_verticalOnesNeedend * 3 / 2) + (3 * i), 0, 0);
+        }
+    }
+
+    void SpawnHorizontal()
+    {
+        for (int i = 0; i < 53/3; i++)
+        {
+            GameObject currentLine = Instantiate(_lineH);
+            currentLine.transform.position = new Vector3(0, 0, i * 3);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
